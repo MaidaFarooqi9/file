62d97b9ca6ef619f16918333a08f9f21dbd3c74b
Maida Farooqi <34773861+MaidaFarooqi9@users.noreply.github.com>
2020-06-15 01:40:59 +0500

    Add files via upload

--- main.dart
+++ main.dart
@@ -1,58 +1,97 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
-import 'ProductModel.dart';
-import 'ProductScreen.dart';
-import 'CheckoutScreen.dart';
+import 'package:mandi/goat.dart';
+import 'package:mandi/login.dart';
+import 'package:mandi/registration.dart';
+import 'package:mandi/buyerPage.dart';
+import 'cow.dart';
+
+void main() => runApp(MaterialApp(//home:MyApp()
+    initialRoute: '/',
+    routes: {
+      '/': (context) => MyApp(),
+      '/login': (context) => login(),
+      '/RegisterPage':(context)=>register(),
+      '/BuyerPage':(context)=>buyer(),
+      '/cow':(context)=>cow(),
+      '/goat':(context)=>goat(),
+    }
 
 
+));
 
-
-void main() => runApp(MaterialApp(home:MainScreen()));
-
-
-class MainScreen extends StatefulWidget {
-  @override
-  _MainScreenState createState() => _MainScreenState();
-}
-
-class _MainScreenState extends State<MainScreen> {
-  List<ProductModel> cart = [];
-  int sum = 0;
-
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return DefaultTabController(
-      length: 2,
-      child: Scaffold(
-        appBar: AppBar(
-          title: Text("Cart App"),
-          bottom: new TabBar(
-            tabs: <Widget>[
-             new Tab(text: "Products",),
-              new Tab(text: "Checkout",),
-            ],
-          ),
-        ),
-        body: TabBarView(
+
+
+
+    return Scaffold(
+      appBar:AppBar(title:Text('Mandi'),backgroundColor:Colors.teal,) ,
+
+      body:Container(
+   /*decoration:BoxDecoration(
+    image:DecorationImage(image: AssetImage
+      ("images/bg.png"),
+      fit:BoxFit.cover,
+
+    ),
+    ),*/
+        child: Column(
+          mainAxisAlignment:MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
           children: <Widget>[
-            ProductScreen((selectedProduct){
-              setState(() {
-                cart.add(selectedProduct); //update
-                sum = 0;
-                cart.forEach((item){
-                  sum = sum + item.price;
-                });
-              });
-            },(unSelectedProduct){setState(() {
-              cart.remove(unSelectedProduct);
-              //update
-              sum=sum-unSelectedProduct.price;
-            });
-            }
-              ),
-            CheckoutScreen(cart, sum),
+        ClipRRect(
+        borderRadius: BorderRadius.circular(136.0),
+            child:Image.asset('images/cow.png',height:95.0,width:85.0,),),
+
+            ShaderMask(
+              blendMode: BlendMode.srcATop,
+              shaderCallback: (rect) {
+                return LinearGradient(
+
+                  colors:<Color>[Colors.green, Colors.red],
+                  tileMode: TileMode.mirror,
+                ).createShader(rect);
+              },
+              child: Center(child: const Text('Who are you?',style: TextStyle(fontSize: 28,fontWeight:FontWeight.bold),)),
+            ),
+
+            ShaderMask(
+              blendMode: BlendMode.srcATop,
+              shaderCallback: (rect) {
+                return LinearGradient(
+                  colors:[Colors.red, Colors.pink.shade900],
+                  tileMode: TileMode.mirror,
+                ).createShader(rect);
+              },
+              child:  FlatButton(onPressed:(){Navigator.pushNamed(context, '/BuyerPage');},child:Text('A Buyer',style: TextStyle(fontSize: 27,fontWeight:FontWeight.bold),),),
+            ),
+
+            ShaderMask(
+              blendMode: BlendMode.srcATop,
+              shaderCallback: (rect) {
+                return LinearGradient(
+                  colors:[Colors.red, Colors.pink.shade900],
+                  tileMode: TileMode.mirror,
+                ).createShader(rect);
+              },
+              child:  FlatButton(onPressed:(){Navigator.pushNamed(context, '/login');},
+                child:Text('A Seller',style: TextStyle(fontSize: 27,fontWeight:FontWeight.bold),),),
+            ),
+
+
+
+
+
+
           ],
         ),
       ),
+
+
+
     );
   }
 }
